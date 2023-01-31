57a4e7dc0e77908781945e3b309007eafa98bd3d
Moelmi89 <moelmi89@gmail.com>
2023-01-31 23:28:33 +0000

    quiz js added to repo

---
+++ assets/js/quiz.js
@@ -0,0 +1,18 @@
+var startQuiz = document.getElementById("startQuiz");
+var currentScore = document.getElementById("currentScore");
+var question = document.getElementById("question");
+var option = document.getElementById("option");
+var message = document.getElementById("message");
+
+var results = document.getElementById("results");
+var viewScores = document.getElementById("viewScores");
+var submit = document.getElementById("submit");
+var welcome = document.getElementById("welcome");
+var initials = document.getElementById("initials");
+var finalScore = document.getElementById("finalScore");
+var playAgain = document.getElementById("play-again");
+
+var highScores = document.getElementById("highScores");
+var highScoreList = document.getElementById("highScoreList");
+
+var timer = document.getElementById("timer");
